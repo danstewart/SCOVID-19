7889de578a7d6c0fd603d1909e95b42f50b79b73
Dan Stewart <danielandrewstewart@gmail.com>
2020-04-13 14:20:12 +0000

    Fix run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ today=$(date +%Y-%m-%d)
 
 source "$APP_ROOT/venv/bin/activate"
 python3 "$APP_ROOT/bin/scrape.py" > "$APP_ROOT/out/${today}.json"
-python3 "$APP_ROOT/bin/combine.py" > "$APP_ROOT/site/breakdown.json"
+python3 "$APP_ROOT/bin/combine.py"
 deactivate
