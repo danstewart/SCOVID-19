7b37202f368eb97b34b0ed65cf53e199da68e9a3
Dan Stewart <github@mail.danstewart.dev>
2020-06-07 16:12:31 +0000

    Save stats page to wayback machine

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Save it to the wayback machine
+curl -s -q "https://web.archive.org/save/https://www.gov.scot/publications/coronavirus-covid-19-daily-data-for-scotland/" > /dev/null
+
 [[ -z $PROJECT_ROOT ]] && PROJECT_ROOT="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 
 # Load the .env
@@ -10,6 +13,8 @@ today=$(date +%Y-%m-%d)
 source "$PROJECT_ROOT/venv/bin/activate"
 python3 "$PROJECT_ROOT/src/scrape.py" > "$PROJECT_ROOT/data/${today}.json"
 python3 "$PROJECT_ROOT/src/site-gen.py"
+
 deactivate
 
 terser --compress --mangle --output "$PROJECT_ROOT/site/script.js" -- "$PROJECT_ROOT/site/script.js"
+
